c1fd23fdcb91cf7b0fc47b31444c85c7ece33b34
Rafli Malik <Rafli.Malik@gmail.com>
2026-03-10 12:57:48 +0700

    style: update footer

--- apps/web/src/components/footer.tsx
+++ apps/web/src/components/footer.tsx
@@ -1,8 +1,18 @@
 export function Footer() {
 	return (
-		<footer className="flex flex-col items-center justify-center bg-foreground p-4 text-center text-sm text-background">
-			<p>&copy; {new Date().getFullYear()} Rafli Malik</p>
-			<a href="https://github.com/RMalik777/url-shortener/blob/main/LICENSE">
+		<footer className="flex flex-col items-center justify-center bg-muted p-4 text-center text-sm">
+			<p>
+				&copy; {new Date().getFullYear()}{" "}
+				<a href="http://raflimalik.com" target="_blank" rel="noopener noreferrer">
+					Rafli Malik
+				</a>
+			</p>
+			<a
+				className="text-muted-foreground hover:text-foreground hover:underline"
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://github.com/RMalik777/url-shortener/blob/main/LICENSE"
+			>
 				Licensed under AGPL v3.0
 			</a>
 		</footer>
