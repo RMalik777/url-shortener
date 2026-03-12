5c0ded3f293f4b81719be3fc09d7e71e43442ab5
Rafli Malik <Rafli.Malik@gmail.com>
2026-03-12 13:50:04 +0700

    chore: change web name

--- apps/web/src/routes/(auth)/signin.tsx
+++ apps/web/src/routes/(auth)/signin.tsx
@@ -35,7 +35,7 @@ function RouteComponent() {
 									<div className="flex flex-col items-center gap-2 text-center">
 										<h1 className="text-2xl font-bold">Welcome back</h1>
 										<p className="text-balance text-muted-foreground">
-											Login to your Acme Inc account
+											Login to your URL Shortener account
 										</p>
 									</div>
 									{errorMessage && (
