38866348d758c1398602c9f41ea95860b2344112
Rafli Malik <Rafli.Malik@gmail.com>
2026-03-13 10:44:29 +0700

    chore: add header for link-removed page

--- apps/web/src/routes/(app)/(splash)/link-removed/index.tsx
+++ apps/web/src/routes/(app)/(splash)/link-removed/index.tsx
@@ -4,6 +4,18 @@ import { Button } from "@repo/ui/components/button";
 import { LinkRemovedTemplate } from "@repo/ui/template/link-removed";
 
 export const Route = createFileRoute("/(app)/(splash)/link-removed/")({
+	headers: () => ({
+		"Cache-Control": "public, max-age=3600, stale-while-revalidate=86400",
+	}),
+	head: () => ({
+		title: "Link Removed | URL Shortener",
+		meta: [
+			{
+				name: "description",
+				content: "The link you are trying to access has been removed.",
+			},
+		],
+	}),
 	component: RouteComponent,
 });
 
