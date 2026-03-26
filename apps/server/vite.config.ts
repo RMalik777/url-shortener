114457adc70de97d01abebab193b5398385af9dc
Rafli Malik <Rafli.Malik@gmail.com>
2026-03-26 15:07:44 +0700

    chore: update to vite 8 and react plugin 6

--- apps/server/vite.config.ts
+++ apps/server/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
-import viteTsConfigPaths from "vite-tsconfig-paths";
+import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
@@ -10,20 +10,16 @@ const config = defineConfig({
 	server: {
 		port: 5173,
 	},
+	resolve: {
+		tsconfigPaths: true,
+	},
 	plugins: [
 		cloudflare({ viteEnvironment: { name: "ssr" }, inspectorPort: 9231 }),
-		// this is the plugin that enables path aliases
-		viteTsConfigPaths({
-			projects: ["./tsconfig.json"],
-		}),
 		devtools(),
 		tailwindcss(),
 		tanstackStart(),
-		viteReact({
-			babel: {
-				plugins: ["babel-plugin-react-compiler"],
-			},
-		}),
+		viteReact(),
+		babel({ presets: [reactCompilerPreset()] }),
 	],
 });
 
